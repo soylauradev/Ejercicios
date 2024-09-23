711b2502519ed69031b4e1bdd63a634fc0369453
soylauradev <lauragutierrezceballos@gmail.com>
2024-09-23 20:17:31 +0200

    segunda actualización variables

--- JS Basic Algorithms/Iteración Variables avanzadas/app.js
+++ JS Basic Algorithms/Iteración Variables avanzadas/app.js
@@ -11,15 +11,15 @@ Muestralos por consola de esta forma:
     let lastName = "Snow";
     let age = 24;
 
-    let presentacion = "Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos"
-    console.log (presentacion);
+    let presentacion = `Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`
+    console.log(presentacion);
 
     /* 1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
 ambos juguetes. */
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
 
-console.log (toy1.price + toy2.price);
+console.log(toy1.price + toy2.price);
 
 
 /* 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
